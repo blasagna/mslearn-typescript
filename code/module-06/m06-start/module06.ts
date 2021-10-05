a46ba262a0ff14198928c18b087e8fca2ed1106c
Bob DiMaiolo <bdimaiolo@gmail.com>
2021-10-05 08:37:21 -0400

    generics

--- code/module-06/m06-start/module06.ts
+++ code/module-06/m06-start/module06.ts
@@ -3,17 +3,16 @@
 
 /*  DataStore is a utility function that can store up to 10 string values in an array. 
     Rewrite the DataStore class so the array can store items of any type.
+*/
+class DataStore<T> {
 
-    TODO: Add and apply a type variable. */
-class DataStore {
-
-    private _data = new Array(10);
+    private _data: Array<T> = new Array(10);
     
-    AddOrUpdate(index: number, item: string) {
+    AddOrUpdate(index: number, item: T) {
         if(index >=0 && index <10) {
             this._data[index] = item;
         } else {
-            alert('Index is greater than 10')
+            console.error('Index is greater than 10')
         }
     }
     GetData(index: number) {
@@ -25,7 +24,7 @@ class DataStore {
     }
 }
 
-let cities = new DataStore();
+let cities = new DataStore<string>();
 
 cities.AddOrUpdate(0, "Mumbai");
 cities.AddOrUpdate(1, "Chicago");
@@ -34,7 +33,20 @@ cities.AddOrUpdate(11, "London");       // item not added
 console.log(cities.GetData(1));         // returns 'Chicago'
 console.log(cities.GetData(12));        // returns 'undefined'
 
-// TODO Test items as numbers.
+// Test items as numbers.
+let empIDs = new DataStore<number>();
+empIDs.AddOrUpdate(0, 50);
+empIDs.AddOrUpdate(1, 65);
+empIDs.AddOrUpdate(2, 89);
+console.log(empIDs.GetData(0));
 
-
-// TODO Test items as objects.
+// Test items as objects.
+type Pets = {
+    name: string;
+    breed: string;
+    age: number
+}
+let pets = new DataStore<Pets>();
+pets.AddOrUpdate(0, { name: 'Rex', breed: 'Golden Retriever', age: 5});
+pets.AddOrUpdate(1, { name: 'Sparky', breed: 'Jack Russell Terrier', age: 3});
+console.log(pets.GetData(1));         // returns { name: 'Sparky', breed: 'Jack Russell Terrier', age: 3 }
